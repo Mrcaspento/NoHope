b23de73bf69de6ce895198c767939eade7240a8e
Caspento <Mrcaspento@gmail.com>
2020-11-03 18:13:26 -0500

    adding a populate btn function

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+function populate(){
+    if(quiz.isDone()){
+        //showScores();
+    }else {
+        //show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.questionIndex().text;
+
+    }//show choices
+    var choices = quiz.questionIndex().choices;
+    for(var i = 0; i< choices.length; i++){
+        var element = document.getElementById("choice" + i); // this way it chooses all choices i have 
+        element.innerHTML = choices[i];
+    }
+}   
+  var quiz = new Quiz(questions);
+  //function that operates the questions
+  populate();
+
 var questions = [
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
-    new question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
+    new Question("placeholder question?", ["placeholder answer","placeholder answer","placeholder answer","placeholder answer"],"correctanswer"),
 ];
-   
-  
--- question.js
+++ question.js
@@ -1,29 +1,9 @@
-// world variables
-function question(text, choices, answer){ // storing these parameters into the variables below
-  this.text= text;
-  this.choice = choices;
+function Question(text, choices, answer){
+  this.text =text;
+  this.choices = choices;
   this.answer = answer;
-
 }
-//made the  
-question.prototype.correctAnswer = function(choice){
+
+Question.prototype.correctAnswer = function (choice) {
   return choice === this.answer;
-}
-
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.questionIndex = 0;
-}
-Quiz.prototype.getQuestion = function(){
-  return this.questions[this.questionIndex];
-}
-Quiz.prototype.isDone = function(){
-  return this.questions.length === this .questionIndex;
-}
-Quiz.prototype.guess = function(answer){
-  this.questionIndex++;
-  if(this.questionIndex().correctAnswer(answer)) {
-      this.score++;
-  }
 }
