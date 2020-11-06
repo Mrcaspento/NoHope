50383a6b77ad184471800d52bdd6658515867664
Caspento <Mrcaspento@gmail.com>
2020-11-06 17:28:19 -0500

    populating the choicebtns

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 var timerEl = document.getElementById('timer');//timer element
 var contentEl = document.getElementById('content');
 const startButton = document.getElementById('startBtn') 
+const nextButton = document.getElementById('nextBtn') 
 startButton.addEventListener('click', startGame,)
 const quizContainer = document.getElementById('quiz-Container')
 const answerBtnEL = document.getElementById('choiceBtn')
-const randomQuestions, currentQuestionIndex; /// to shuffle the questions
-const questionEl = document.getElementById('question')//gets the question out of the array of questions
+let randomQuestions, currentQuestionIndex; /// to shuffle the questions
+const questionEl = document.getElementById('question-container')//gets the question out of the array of questions
 
 
 
@@ -18,33 +19,53 @@ const questionEl = document.getElementById('question')//gets the question out of
 function startGame(){
     startButton.classList.add('hide');
     quizContainer.classList.remove("hide");
-    randomQuestions = questions.sort(() => Math.random() - .5);
+    randomQuestions = questions.sort(() => Math.random() - .5);// makes sure i get a random question out of questions
     currentQuestionIndex = 0;
-    startTimer();
-    displayQuestion();
+    //startTimer();
+    displayNextQuestion();
 }
-function startTimer();
+//function startTimer();
 
 
 
 function displayNextQuestion(){
-    showQuestion(randomQuestions[currentQuestionIndex])
+    resetQuestion();
+    displayQuestion(randomQuestions[currentQuestionIndex])
 }
-function displayQuestion(question) {
-    questionEl.innerText = questions.question
+function displayQuestion(questions) {
+    questionEl.innerText = questions.question;
+    questions.choices.forEach(choices => {
+        const button = document.createElement('button')
+        button.innerText = choices.text
+        button.classList.add('choiceBtn')
+        if(choices.answer){
+            button.dataset.answer = choices.answer
+        }
+        button.addEventListener('click', selectChoice)
+        answerBtnEL.appendChild(button)
+    });
 }
+function resetQuestion(){
+    nextButton.classList.add('hide')
+    while(answerBtnEL.firstChild){
+        answerBtnEL.removeChild
+    }
+}
+//function selectChoice(e){
+
+//}
 
 
 //--------------------------------------------------------------------------------------------------------------------------------
-
+//Question array with choices and correct answer
 const questions = [
     {
-        question: "placeholder question?",
+        question: "what is javaScript?",
         choices: [
-             "Placeholder answer",
-             "Placeholder answer",
-             "Placeholder answer",
-             "Placeholder answer"
+             "Placehasdfaolder danswer",
+             "Placeholasdfasdfasder dsaanswer",
+             "Placeholderadsfasdf answer",
+             "Placeholadsfasdfder answer"
         ],
         answer: 0 /// the correct answer
     }, {
@@ -111,6 +132,6 @@ const questions = [
         ],
         answer: 0 /// the correct answer
     },]
-v
+
 
      
