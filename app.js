e3374445fbd0964cc0f7f4614f44c24a464997c3
Caspento <Mrcaspento@gmail.com>
2020-11-06 18:30:29 -0500

    finished making the quiz be able to have random questions and the answers populating the button choices and added a next button next is add the timer clock

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 ///GLOBAL VARS
 
 
-const startButton = document.getElementById('startBtn') 
-const nextButton = document.getElementById('nextBtn') 
+const startButton = document.getElementById('startBtn')
+const nextButton = document.getElementById('nextBtn')
 startButton.addEventListener('click', startGame,)
 const quizContainer = document.getElementById('quiz-Container')
 const choiceBtnEL = document.getElementById('choice-buttons')
 let randomQuestions, currentQuestionIndex; /// to shuffle the questions
 const questionEl = document.getElementById('question-container')//gets the question out of the array of questions
 
-
-
-//--------------------------------------------------------------------------------------------------------------
+nextButton.addEventListener('click',() =>{
+    currentQuestionIndex++;
+    displayNextQuestion();
+ })
+startButton.addEventListener('click', startGame,)
+////--------------------------------------------------------------------------------------------------------------
 //
 //-----------------------------------------------------------------------------------------------------------------------------
 //The Timer and quiz start functions!!!
 
-function startGame(){
+function startGame() {
     startButton.classList.add('hide');
     quizContainer.classList.remove("hide");
     randomQuestions = questions.sort(() => Math.random() - .5);// makes sure i get a random question out of questions
@@ -28,31 +31,50 @@ function startGame(){
 
 
 
-function displayNextQuestion(){
-   // resetQuestion();
+function displayNextQuestion() {
+    resetQuestion()
     displayQuestion(randomQuestions[currentQuestionIndex])
+
 }
 function displayQuestion(questions) {
     questionEl.innerText = questions.question;
-    question.choices.forEach(choice => {
+    questions.choices.forEach(choice => {
         const button = document.createElement('button')
-        button.innerText = choices.text
-        button.classList.add('choiceBtn')
-        if(choice.correct){
+        button.innerText = choice.text
+        button.classList.add('btn')
+        if (choice.correct) {
             button.dataset.correct = choice.correct
         }
         button.addEventListener('click', selectChoice)
-        answerBtnEL.appendChild(button)
-    });
+        choiceBtnEL.appendChild(button)
+    })
 }
-function resetQuestion(){
+function resetQuestion() {
     nextButton.classList.add('hide')
-    while(answerBtnEL.firstChild){
-        answerBtnEL.removeChild
+    while (choiceBtnEL.firstChild) {
+        choiceBtnEL.removeChild(choiceBtnEL.firstChild)
     }
 }
-function selectChoice(event){
-
+function selectChoice(e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStat(document.body, correct)
+    Array.from(choiceBtnEL.children).forEach(button =>{
+        setStat(button, button.dataset.correct)
+    })
+    nextButton.classList.remove('hide')
+}
+function setStat(element, correct){
+    clearStat(element)
+    if(correct){
+        element.classList.add('correct')
+    }else{
+        element.classList.add('wrong')
+    }
+}
+function clearStat(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 
@@ -62,93 +84,92 @@ const questions = [
     {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-             {text:'Placehasdfaolder danswer', correct: true},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
-             {text:'Placehasdfaolder danswer', correct: false},
+            { text: 'Placehasdfaolder danswer', correct: true },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: 'Placehasdfaolder danswer', correct: false }
         ]
     },
 ]
 
 
-     
