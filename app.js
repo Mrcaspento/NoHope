260922b8e76ec96766dc6b03853c232c03801ae6
Caspento <Mrcaspento@gmail.com>
2020-11-06 20:52:11 -0500

    made another html and js file

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const quizContainer = document.getElementById('quiz-Container')
 const choiceBtnEL = document.getElementById('choice-buttons')
 let randomQuestions, currentQuestionIndex; /// to shuffle the questions
 const questionEl = document.getElementById('question-container')//gets the question out of the array of questions
-
-nextButton.addEventListener('click',() =>{
+var myTimer = document.getElementById('timer')
+let highScore = document.getElementById('highscore')
+nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
     displayNextQuestion();
- })
-startButton.addEventListener('click', startGame,)
+})
+startButton.addEventListener('click', startGame, startTimer)
 ////--------------------------------------------------------------------------------------------------------------
-//
+var time = setInterval(startTimer, 1000);
+var score = "";
+var seconds = 60;
+var scoreArrg = JSON.parse(localStorage.getItem("highscores")) || []
+
+//start timer
+
+
+
 //-----------------------------------------------------------------------------------------------------------------------------
 //The Timer and quiz start functions!!!
 
@@ -27,14 +36,34 @@ function startGame() {
     //startTimer();
     displayNextQuestion();
 }
-//function startTimer();
+questionContainer.addEventListener('click',function(eventT)){
+    eventT.preventDefault();
+    if(eventT.target.matches('button)'))
+    var userAnswer = eventT.target.getAttribute('id')
+    if(userAnswer != questions[currentQuestionIndex -1].correct){
+        secondsLeft -= 10
+    }
+    if(currentQuestionIndex === questions.length) {
+        score = secondsLeft
+        localStorage.setItem('highscores',JSON.stringify(score))
+        location.href-"highscores.html"
+    }
 
+}
+function startTimer(seconds) {
+    timerEl = seconds + "seconds left";
+    seconds--;
+    if (seconds == -1) {
+        clearInterval(time);
+        alert("NO SOUP FOR YOU!");
+    }
+}
 
-
+//-----------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
 function displayNextQuestion() {
     resetQuestion()
     displayQuestion(randomQuestions[currentQuestionIndex])
-
 }
 function displayQuestion(questions) {
     questionEl.innerText = questions.question;
@@ -60,25 +89,26 @@ function selectChoice(e) {
     const selectButton = e.target
     const correct = selectButton.dataset.correct
     setStat(document.body, correct)
-    Array.from(choiceBtnEL.children).forEach(button =>{
+    Array.from(choiceBtnEL.children).forEach(button => {
         setStat(button, button.dataset.correct)
     })
-    if(randomQuestions.length > currentQuestionIndex + 1){
-    nextButton.classList.remove('hide')
-    }else{
+    if (randomQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
         startButton.innerText = 'restart'
         startButton.classList.remove('hide')
     }
 }
-function setStat(element, correct){
+function setStat(element, correct) {
     clearStat(element)
-    if(correct){
+    if (correct) {
         element.classList.add('correct')
-    }else{
+
+    } else {
         element.classList.add('wrong')
     }
 }
-function clearStat(element){
+function clearStat(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
@@ -107,57 +137,57 @@ const questions = [
         question: "what does ceil() method do?",
         choices: [
             { text: 'rounds a number upwards to the nearest interger', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
+            { text: '', correct: false },
             { text: 'nothing', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
             { text: 'this ', correct: true },
             { text: 'drugs', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: false },
+            { text: '', correct: false }
+        ]
+    }, {
+        question: "do you hate Tom Cruise",
+        choices: [
+            { text: 'katie holmes does ', correct: true },
+            { text: 'Pizza', correct: false },
+            { text: 'orangutan', correct: false },
+            { text: '', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-            { text: 'Placehasdfaolder danswer', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: true },
+            { text: '', correct: false },
+            { text: '', correct: false },
+            { text: '', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-            { text: 'Placehasdfaolder danswer', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: true },
+            { text: '', correct: false },
+            { text: '', correct: false },
+            { text: '', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-            { text: 'Placehasdfaolder danswer', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: true },
+            { text: '', correct: false },
+            { text: '', correct: false },
+            { text: '', correct: false }
         ]
     }, {
         question: "what is javaScript?",
         choices: [
-            { text: 'Placehasdfaolder danswer', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
-        ]
-    }, {
-        question: "what is javaScript?",
-        choices: [
-            { text: 'Placehasdfaolder danswer', correct: true },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false },
-            { text: 'Placehasdfaolder danswer', correct: false }
+            { text: '', correct: true },
+            { text: '', correct: false },
+            { text: '', correct: false },
+            { text: '', correct: false }
         ]
     }
 ]
